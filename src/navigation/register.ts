89a9e70e273739e97855c2659d17bffd8bca5116
Baranidharan Thiruvengadam <btbrad1979@gmail.com>
2021-02-28 22:30:09 +1100

    - Remove sectionList

--- src/navigation/register.ts
+++ src/navigation/register.ts
@@ -1,14 +1,12 @@
 import { Navigation } from 'react-native-navigation';
 import Profile from '../screens/profile';
 import Home from '../screens/home';
-import UISectionList from '../components/UISectionList';
 import UITopbar from '../components/UITopbar';
 
 
 const RegisterAllScreens = () => {
     Navigation.registerComponent('ProfileScreen', () => Profile);
     Navigation.registerComponent('HomeScreen', () => Home);
-    Navigation.registerComponent('SectionList', () => UISectionList);
     Navigation.registerComponent('TopBar', () => UITopbar);
 }
 
